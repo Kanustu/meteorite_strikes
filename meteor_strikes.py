ea7c3403b8c8d4121cfe6dbb5f2201ea162e2521
JDKanius <kanustu@gmail.com>
2025-03-05 09:37:57 -0600

    adjusted notebooks 02/26/25

--- meteor_strikes.py
+++ meteor_strikes.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 db = pd.read_csv('Meteorite_Landings.csv')
+meteor_lat = []
+meteor_long = []
 for num in range(len(db['reclat'].head(5))):
-    print(db['reclat'][num], db['reclong'][num])
+    meteor_lat.append(db['reclat'][num])
+    meteor_long.append(db['reclong'][num])
+print(meteor_lat[0], meteor_long[0])
